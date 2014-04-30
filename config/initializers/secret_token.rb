8658b51285fd9b3d695913383d4eb6177353196d
Rodrigo Silva dos Santos <rodrigos.santos01@gmail.com>
2014-04-30 20:44:58 -0300

    created rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+BrainRails::Application.config.secret_key_base = 'c7cf7fa91897a6153f2cbaee11710f599f6383605662a7030cf2891a6d76213e1514b0208e14ca45cb9aa1310f846c91ee588cf5c8cf74667a299882eff70419'
